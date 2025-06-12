cd2efa1e6e14f44d42ee3f9f98f66c949d2398b9
Deliplus <102957136+Deliplus@users.noreply.github.com>
2025-06-12 14:01:42 +0200

    Update mistral_generate.py

--- mistral_generate.py
+++ mistral_generate.py
@@ -2,14 +2,15 @@ import requests
 import os
 # replace with key from https://openrouter.ai/
 MODEL = "mistralai/mistral-7b-instruct"
-api_key = os.getenv("OPENROUTER_API_KEY")
+api_key = os.getenv("OPENROUTER_API_KEY")  
+
 def mistral_translate(source_text: str) -> str:
     prompt = f"[ONLY RETURN CATALAN TRANSLATION, NO EXPLANATION]\nTranslate to CATALAN:\n{source_text}"
-
     headers = {
-        "Authorization": f"Bearer {OPENROUTER_API_KEY}",
-        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}",  
+        "Content-Type": "application/json"
     }
+
 
     body = {
         "model": MODEL,
