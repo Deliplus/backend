988754f89e2a573cf2ff0e3342912ed971a39ae8
Deliplus <102957136+Deliplus@users.noreply.github.com>
2025-06-13 12:40:14 +0200

    Update setup_db.py

--- setup_db.py
+++ setup_db.py
@@ -2,7 +2,8 @@
 from db import Base, engine
 from models.evaluation import Evaluation
 from models.user_progress import UserProgress
-
+print('delete tabe')
+Base.metadata.drop_all(bind=engine) 
 print("📦 Creating tables...")
 Base.metadata.create_all(bind=engine)
 print("✅ Done.")
